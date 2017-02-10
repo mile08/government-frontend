d4f8c5bf238cb214a060e976e9ac73a417fa2492
Andrew Hilton <igberty@gmail.com>
2017-02-10 11:06:07 +0000

    Minimal set of copied files to render contacts-frontend in government-frontend
    we are migrating over contacts-frontend to government-frontend
    
    this is the minimal set of files to render within government-frontend
    the only refactoring that took place was some renaming of varibales and
    file paths tog et it to work, no refactoring

---
+++ app/presenters/contact_presenter.rb
@@ -0,0 +1,80 @@
+class ContactPresenter < ContentItemPresenter
+ 
+  PASS_THROUGH_KEYS = [
+    :title, :details, :links, :web_url
+  ].freeze
+
+  PASS_THROUGH_DETAILS_KEYS = [
+    :description, :query_response_time,
+    :more_info_contact_form, :more_info_email_address, :more_info_post_address, :more_info_phone_number
+  ].freeze
+
+  PASS_THROUGH_COLLECTIONS_KEYS = [
+    :quick_links, :phone_numbers, :email_addresses, :post_addresses,
+    :contact_form_links
+  ].freeze
+
+  PASS_THROUGH_KEYS.each do |key|
+    define_method key do
+      content_item[key.to_s]
+    end
+  end
+
+  PASS_THROUGH_DETAILS_KEYS.each do |key|
+    define_method key do
+      details[key.to_s] if details
+    end
+  end
+
+  PASS_THROUGH_COLLECTIONS_KEYS.each do |key|
+    define_method key do
+      details[key.to_s].map { |item| OpenStruct.new(item) } if details
+    end
+  end
+
+  def format
+    content_item["format"]
+  end
+
+  def organisation
+    @content_item["organisation"]
+  end
+
+  def organisation
+    # The only organisation in here is HMRC, so call `.first` on the array.
+    content_item["links"].fetch("organisations", []).map { |organisation| OpenStruct.new(organisation) }.first
+  end
+
+  def slug
+    URI.parse(content_item["web_url"]).path.sub(%r{\A/}, "")
+  end
+
+  def related_links
+    return [] unless content_item["links"]
+    content_item.to_hash.dig("links", "related").to_a
+  end
+
+  def updated_at
+    date = @content_item["updated_at"]
+    DateTime.parse(date) if date
+  end
+
+  def show_webchat?
+    # These are the routes on which we plan to roll out webchat, in stages.
+    whitelisted_paths = [
+      '/government/organisations/hm-revenue-customs/contact/child-benefit',
+      '/government/organisations/hm-revenue-customs/contact/income-tax-enquiries-for-individuals-pensioners-and-employees',
+      '/government/organisations/hm-revenue-customs/contact/vat-online-services-helpdesk',
+      '/government/organisations/hm-revenue-customs/contact/national-insurance-numbers',
+      '/government/organisations/hm-revenue-customs/contact/self-assessment-online-services-helpdesk',
+      '/government/organisations/hm-revenue-customs/contact/self-assessment',
+      '/government/organisations/hm-revenue-customs/contact/tax-credits-enquiries',
+      '/government/organisations/hm-revenue-customs/contact/vat-enquiries',
+      '/government/organisations/hm-revenue-customs/contact/customs-international-trade-and-excise-enquiries',
+      '/government/organisations/hm-revenue-customs/contact/trusts',
+      '/government/organisations/hm-revenue-customs/contact/employer-enquiries',
+      '/government/organisations/hm-revenue-customs/contact/construction-industry-scheme',
+    ]
+    whitelisted_paths.include?(@content_item["base_path"])
+  end
+end
